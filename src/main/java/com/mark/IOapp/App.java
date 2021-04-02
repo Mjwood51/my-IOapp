62a492367846af2af941ceeebf67b229b31a0397
Mark Woodard <MarkWoodard2227@gmail.com>
2021-04-02 19:49:58 -0400

    initial project setup

---
+++ src/main/java/com/mark/IOapp/App.java
@@ -0,0 +1,13 @@
+package com.mark.IOapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
